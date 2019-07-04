5238e601ee1174363e1c0ba00206909067122945
flodu13 <50022982+flodu13@users.noreply.github.com>
2019-07-04 15:51:41 +0200

    Bdd implementation (#2)
    * Ajout des implementations bdd
    
    * Avec documentation

--- src/main/java/fr/formation/artist/Artist.java
+++ src/main/java/fr/formation/artist/Artist.java
@@ -31,58 +31,130 @@ public class Artist {
     @Column(name = "imageType")
     private String imageType;
 
+
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
+
     public Long getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * Gets email.
+     *
+     * @return the email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets email.
+     *
+     * @param email the email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Gets city.
+     *
+     * @return the city
+     */
     public String getCity() {
         return city;
     }
+
+    /**
+     * Sets city.
+     *
+     * @param city the city
+     */
 
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets description.
+     *
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets description.
+     *
+     * @param description the description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets image.
+     *
+     * @return the image
+     */
     public String getImage() {
         return image;
     }
 
+    /**
+     * Sets image.
+     *
+     * @param image the image
+     */
     public void setImage(String image) {
         this.image = image;
     }
-
+    /**
+     * Gets imageType.
+     *
+     * @return the imageType
+     */
     public String getImageType() {
         return imageType;
     }
 
+    /**
+     * Sets imageType.
+     *
+     * @param imageType the imageType
+     */
     public void setImageType(String imageType) {
         this.imageType = imageType;
     }
