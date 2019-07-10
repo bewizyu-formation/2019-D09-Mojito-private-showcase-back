2c72041ebfd9a79f99499b7ca3b296a96c5cb114
Alexandre MEUR <alex.meur@gmail.com>
2019-07-10 12:38:42 +0200

    Added a route to UserController to list all users
    use localhost:8080/users/all to get all users

--- src/main/java/fr/formation/user/UserController.java
+++ src/main/java/fr/formation/user/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * The type User controller.
  */
@@ -38,5 +40,11 @@ public class UserController {
 		return userService.getListSize();
 
 	}
+
+	@GetMapping("all")
+	public List<User> findAll(){
+		return userService.findAll();
+	}
+
 
 }
