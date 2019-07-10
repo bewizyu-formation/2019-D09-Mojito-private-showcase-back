8a09d1e7eef638debb5f99f60f3d2ff1d44bf99a
florent ros <ros-florent@hotmail.fr>
2019-07-10 11:10:40 +0200

    Config Spring Vote

--- src/main/java/fr/formation/votes/VoteController.java
+++ src/main/java/fr/formation/votes/VoteController.java
@@ -1,4 +1,35 @@
 package fr.formation.votes;
 
+
+import fr.formation.artist.Artist;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/vote")
 public class VoteController {
+
+    @Autowired
+    private VoteService voteService;
+
+    @GetMapping(value = "/",produces = "application/json")
+    public List<Vote> listVote(){
+
+        List<Vote> listVote =  voteService.listVotes();
+
+        return listVote;
+    }
+
+    @PutMapping(value = "/", consumes = "application/json")
+    public boolean registerVote(@RequestBody Vote newVote,@RequestParam int note) {
+        return voteService.addVote(newVote);
+    }
+
+    @PostMapping(value = "/note/{note}", consumes = "application/json")
+    public boolean modifyNote(@PathVariable Long id,@RequestBody Vote newVote) {
+        return voteService.modifyVote(newVote, id);
+    }
 }
