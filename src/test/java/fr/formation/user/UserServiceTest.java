f0d486bc9ba8cda988d228a726f0a8a947f4d907
clesauln <clesauln@protonmail.com>
2019-07-15 14:18:26 +0200

    add artist to event

--- src/test/java/fr/formation/user/UserServiceTest.java
+++ src/test/java/fr/formation/user/UserServiceTest.java
@@ -1,5 +1,8 @@
 package fr.formation.user;
 
+import fr.formation.artist.Artist;
+import fr.formation.artist.ArtistRepository;
+import fr.formation.artist.ArtistService;
 import fr.formation.event.Event;
 import fr.formation.event.EventDaoRepository;
 import fr.formation.event.EventService;
@@ -19,13 +22,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class UserServiceTest {
 
+
+    @Mock
+    private ArtistRepository artistRepository;
 
     @Mock
     private UserRepository userRepository;
@@ -33,11 +36,15 @@ public class UserServiceTest {
     private EventDaoRepository EventDaoRepository;
     @Mock
     private PasswordEncoder passwordEncoder;
+
     @InjectMocks
     private UserService userService;
 
-    @InjectMocks
+    @Mock
     private EventService eventService;
+
+    @InjectMocks
+    private ArtistService artistService;
 
     @Before
     public void init(){
@@ -55,16 +62,26 @@ public class UserServiceTest {
         user.setId(111L);
         userRepository.save(user);
 
+        Artist artist = new Artist();
+        artist.setEmail("synapson@bandcamp.com");
+        artist.setAdress("8 rue Jaures");
+        artist.setCity("Nantes, Loire Atl.");
+        artist.setDescription("Electro synth pop français");
+        artist.setId(120L);
+
+        artistRepository.save(artist);
+
         Event event = new Event();
         event.setId(10L);
-        event.setOwner(user);
-
-        LocalDateTime date = LocalDateTime.of(LocalDate.of(2019,10,2), LocalTime.of(15,15));
+        event.setArtist(artist);
+        LocalDateTime date = LocalDateTime.of(LocalDate.of(2019, 10, 2), LocalTime.of(15, 15));
         event.setDate(date);
         event.setNbPlace(15);
 
         EventDaoRepository.save(event);
-        userRepository.save(user);
+
+        Mockito.when(artistRepository.findById(120L)).thenReturn(Optional.of(artist));
+        Assertions.assertThat(artistService.artistById(120L)).isEqualTo(artist);
 
         Mockito.when(userRepository.findById(111L)).thenReturn(Optional.of(user));
         Assertions.assertThat(userService.getUserById(111L)).isEqualTo(user);
@@ -74,23 +91,14 @@ public class UserServiceTest {
 
         Mockito.when(userRepository.findById(111L)).thenReturn(Optional.of(user));
 
-        /**userService.addEvent(
-                user.getId(),event
-        );
 
-        Assertions.assertThat(userService.getUserById(111L).getUsername()== "jeanne");
-        Assertions.assertThat(eventService.listEvents().size()==1);
-        Assertions.assertThat(userService.getUserById(111L).getEvents().size()>0);
+        Assertions.assertThat(userService.getUserById(111L).getUsername() == "jeanne");
+        Assertions.assertThat(eventService.listEvents().size() == 1);
+        Assertions.assertThat(userService.getUserById(111L).getEvents().size() > 0);
 
-        /**
-         //EventDaoRepository.save(event);
-         //EventDaoRepository.save(event);
-         userService.addEvent(user.getId(), event);
-         Assertions.assertThat(eventService.listEvents().size()==1);
-         Assertions.assertThat(userService.getUserById(111L).getEvents().size()>0);
-         */
+        }
     }
 
 
 
-}
+
